3f1e549ed4c843bcc35b974ec08bb4c75cb74a4e
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-02 12:27:01 +0200

    fix rimossi log da script git

--- src/git-script/GIT-CHECKOUT.bat
+++ src/git-script/GIT-CHECKOUT.bat
@@ -8,98 +8,94 @@ set DRIVE_GIT_ROOT=C:
 set GIT-ROOT-COMMON=c:\git-common
 set GIT_SOURCETREE=%GIT_ROOT%\FT10-Setup-SourceTree
 set LOCAL_BRANCH_TO_CHECKOUT=master
-set LOG_GIT=%GIT_SOURCETREE%\batch\git_log.txt
-set LOG_GIT_ERR=%GIT_SOURCETREE%\batch\git_error_log.txt
 
 %DRIVE_GIT_ROOT%
 cd /
 
-@echo Starting Git Checkout... > %LOG_GIT% 2> %LOG_GIT_ERR%
-@echo >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo Starting Git Checkout...
 
-
-@echo ---------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out OVERLORD  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo ---------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT_ROOT%/Overlord >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo ---------------------
+@echo Checking out OVERLORD
+@echo ---------------------
+cd %GIT_ROOT%/Overlord
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo ----------------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out ConfiguratoreOverlord  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo ----------------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT_ROOT%/ConfiguratoreOverlord >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo ----------------------------------
+@echo Checking out ConfiguratoreOverlord
+@echo ----------------------------------
+cd %GIT_ROOT%/ConfiguratoreOverlord
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out TheDarkKnight  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT_ROOT%/TheDarkKnight  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out TheDarkKnight
+@echo --------------------------
+cd %GIT_ROOT%/TheDarkKnight
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out TheDarkKnight DeviceDriver  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/TheDarkKnightDeviceDriver  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out TheDarkKnight DeviceDriver
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/TheDarkKnightDeviceDriver
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out TheDarkKnight Setup  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/FT10-Setup  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out TheDarkKnight Setup
+@echo --------------------------  
+cd %GIT-ROOT-COMMON%/FT10-Setup  
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status  
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out TheDarkKnight DeviceDriver Setup  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/FT10Setup-TDKDeviceDriver  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------  
+@echo Checking out TheDarkKnight DeviceDriver Setup
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/FT10Setup-TDKDeviceDriver
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out FTSystem Shutdown  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/FTSystem.ShutDownPC  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out FTSystem Shutdown
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/FTSystem.ShutDownPC
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out RDP  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/FTSystem.RDP  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out RDP
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/FTSystem.RDP
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out FTSystem Communication Framework  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/FTSystemCommunicationFramework  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo -------------------------
+@echo Checking out FTSystem Communication Framework
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/FTSystemCommunicationFramework
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo Checking out UWFLib  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-@echo --------------------------  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-cd %GIT-ROOT-COMMON%/UWFLib.git  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT% >> %LOG_GIT% 2>> %LOG_GIT_ERR%
-call "%GIT_EXE%" status  >> %LOG_GIT% 2>> %LOG_GIT_ERR%
+@echo --------------------------
+@echo Checking out UWFLib
+@echo --------------------------
+cd %GIT-ROOT-COMMON%/UWFLib.git
+call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 cd /
