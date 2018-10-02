1e60c2f072bb1cb0a9d7b9d4ed7d88cf8514ef1c
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-02 13:36:00 +0200

    fix rimossi echo

--- src/git-script/GIT-CHECKOUT.bat
+++ src/git-script/GIT-CHECKOUT.bat
@@ -10,87 +10,87 @@ set GIT-ROOT-COMMON=c:\git-common
 %DRIVE_GIT_ROOT%
 cd /
 
-@echo Starting Git Checkout...
+echo Starting Git Checkout...
 
-@echo ---------------------
-@echo Checking out OVERLORD
-@echo ---------------------
+echo ---------------------
+echo Checking out OVERLORD
+echo ---------------------
 cd %GIT_ROOT%/Overlord
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo ----------------------------------
-@echo Checking out ConfiguratoreOverlord
-@echo ----------------------------------
+echo ----------------------------------
+echo Checking out ConfiguratoreOverlord
+echo ----------------------------------
 cd %GIT_ROOT%/ConfiguratoreOverlord
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------
-@echo Checking out TheDarkKnight
-@echo --------------------------
+echo --------------------------
+echo Checking out TheDarkKnight
+echo --------------------------
 cd %GIT_ROOT%/TheDarkKnight
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------
-@echo Checking out TheDarkKnight DeviceDriver
-@echo --------------------------
+echo --------------------------
+echo Checking out TheDarkKnight DeviceDriver
+echo --------------------------
 cd %GIT-ROOT-COMMON%/TheDarkKnightDeviceDriver
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
 
-@echo --------------------------
-@echo Checking out TheDarkKnight Setup
-@echo --------------------------  
+echo --------------------------
+echo Checking out TheDarkKnight Setup
+echo --------------------------  
 cd %GIT-ROOT-COMMON%/FT10-Setup  
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status  
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------  
-@echo Checking out TheDarkKnight DeviceDriver Setup
-@echo --------------------------
+echo --------------------------  
+echo Checking out TheDarkKnight DeviceDriver Setup
+echo --------------------------
 cd %GIT-ROOT-COMMON%/FT10Setup-TDKDeviceDriver
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------
-@echo Checking out FTSystem Shutdown
-@echo --------------------------
+echo --------------------------
+echo Checking out FTSystem Shutdown
+echo --------------------------
 cd %GIT-ROOT-COMMON%/FTSystem.ShutDownPC
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------
-@echo Checking out RDP
-@echo --------------------------
+echo --------------------------
+echo Checking out RDP
+echo --------------------------
 cd %GIT-ROOT-COMMON%/FTSystem.RDP
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo -------------------------
-@echo Checking out FTSystem Communication Framework
-@echo --------------------------
+echo -------------------------
+echo Checking out FTSystem Communication Framework
+echo --------------------------
 cd %GIT-ROOT-COMMON%/FTSystemCommunicationFramework
-call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT% 
 call "%GIT_EXE%" status
 cd %DRIVE_GIT_ROOT%
 
-@echo --------------------------
-@echo Checking out UWFLib
-@echo --------------------------
+echo --------------------------
+echo Checking out UWFLib
+echo --------------------------
 cd %GIT-ROOT-COMMON%/UWFLib.git
 call "%GIT_EXE%" pull %LOCAL_BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" status
