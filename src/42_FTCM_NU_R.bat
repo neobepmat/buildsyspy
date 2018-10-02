6bfacf3ce0c8ca771064be7ff1e699e879fff0c2
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-02 13:48:44 +0200

    add aggiunta step principali

--- src/42_FTCM_NU_R.bat
+++ src/42_FTCM_NU_R.bat
@@ -31,11 +31,15 @@ SET PATH_MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\
 SET WIX_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.targets
 SET WIXCA_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.ca.targets
 
+ECHO "----GIT CHECKOUT" %0 1>&2
+
 IF [%DO_GIT_CHECKOUT%] == 1 (
 	REM effettua il checkout delle branch GIT
 	SET GIT_ROOT=%ROOT_MOUNTPOINT%
 	call "%BATCH_FOLDER%\git-script\GIT-CHECKOUT.bat" %GIT_ROOT% %BRANCH_TO_CHECKOUT% >> %LOG_FILE% 2>> %LOG_FILE_ERR%
 )
+
+ECHO "----CALL 42_FTCM.BAT" %0 1>&2
 
 CALL "%BATCH_FOLDER%\42_FTCM.BAT"
 
