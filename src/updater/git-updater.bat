d279fd260497c2c68bc68621cc398f4838cdaeb7
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-03 09:14:21 +0200

    fix modificato comportamento git-updater (aggiunto parametro -C)

--- src/updater/git-updater.bat
+++ src/updater/git-updater.bat
@@ -1,4 +1,4 @@
-ECHO "Filename:" %0 1>&2
+@ECHO "Filename:" %0 1>&2
 
 SET LOCAL_BRANCH_TO_CHECKOUT=develop
 IF NOT [%BRANCH_TO_CHECKOUT%] == [] SET LOCAL_BRANCH_TO_CHECKOUT=%BRANCH_TO_CHECKOUT%
@@ -9,23 +9,13 @@ IF NOT [%GIT_ROOT%] == [] SET GIT_ROOT_INTERNAL=%GIT_ROOT%
 rem ------------------------------------------------------------------------
 
 SET GIT_EXE=C:\Program Files (x86)\Git\cmd\git.exe
-set DRIVE_GIT_ROOT=C:
-set GIT-ROOT-COMMON=c:\git-common
+SET GIT_FOLDER=%GIT_ROOT_INTERNAL%/BuildSystem
 
-%DRIVE_GIT_ROOT%
-cd /
+@echo Starting Git Checkout...
 
-echo Starting Git Checkout...
-
-echo ---------------------
-echo Checking out OVERLORD
-echo ---------------------
-cd %GIT_ROOT_INTERNAL%/BuildSystem
-call "%GIT_EXE%" checkout %LOCAL_BRANCH_TO_CHECKOUT%
-call "%GIT_EXE%" pull
-call "%GIT_EXE%" status
-cd %DRIVE_GIT_ROOT%
-
-cd /
-
-PAUSE
+@echo ---------------------
+@echo Checking out BuildSystem for %GIT_ROOT%
+@echo ---------------------
+call "%GIT_EXE%" -C %GIT_FOLDER% checkout %LOCAL_BRANCH_TO_CHECKOUT%
+call "%GIT_EXE%" -C %GIT_FOLDER% pull
+call "%GIT_EXE%" -C %GIT_FOLDER% status
