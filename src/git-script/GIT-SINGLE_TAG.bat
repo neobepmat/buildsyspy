c0e4af29f7e4c6af5d02a2a65138a75168a5746e
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-08 12:11:08 +0200

    fix single git script

--- src/git-script/GIT-SINGLE_TAG.bat
+++ src/git-script/GIT-SINGLE_TAG.bat
@@ -3,11 +3,14 @@
 @echo --------------------->> "%EXT_LOG_FILE%" 2>>1& 
 call "%GIT_EXE%" -C %CURRENT_FOLDER% reset --hard >> "%EXT_LOG_FILE%" 2>>1& 
 call "%GIT_EXE%" -C %CURRENT_FOLDER% checkout %BRANCH_TO_TAG_INTERNAL% >> "%EXT_LOG_FILE%" 2>>1& 
-@ECHO Now Tagging
+@ECHO Now Tagging >> "%EXT_LOG_FILE%" 2>>1& 
+
 IF %DEMO% == 1 (
+	@ECHO -------- Script is in DEMO status --------------------------------------------------- >> "%EXT_LOG_FILE%" 2>>1& 
 	@ECHO This batch has been run in DEMO - next command is not going to be executed >> "%EXT_LOG_FILE%" 2>>1& 
 	@ECHO "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
 )
+
 IF %DEMO% == 0 (
 	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
 )
