cafd22901e752fb5737234f14e3cd33cdbfc8694
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-08 12:34:47 +0200

    chg cambiato output STDERR

--- src/git-script/GIT-SINGLE_TAG.bat
+++ src/git-script/GIT-SINGLE_TAG.bat
@@ -1,15 +1,15 @@
 @echo --------------------->> "%EXT_LOG_FILE%" 2>>1& 
-@echo Checking out %CURRENT_REPO% >> "%EXT_LOG_FILE%" 2>>1& 
+@echo Checking out %CURRENT_REPO% >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
 @echo --------------------->> "%EXT_LOG_FILE%" 2>>1& 
-call "%GIT_EXE%" -C %CURRENT_FOLDER% reset --hard >> "%EXT_LOG_FILE%" 2>>1& 
-call "%GIT_EXE%" -C %CURRENT_FOLDER% checkout %BRANCH_TO_TAG_INTERNAL% >> "%EXT_LOG_FILE%" 2>>1& 
+call "%GIT_EXE%" -C %CURRENT_FOLDER% reset --hard >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
+call "%GIT_EXE%" -C %CURRENT_FOLDER% checkout %BRANCH_TO_TAG_INTERNAL% >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
 @ECHO Now Tagging >> "%EXT_LOG_FILE%" 2>>1& 
 
 IF %DEMO% == 1 (
-	@ECHO This batch has been run in DEMO - next command is not going to be executed >> "%EXT_LOG_FILE%" 2>>1& 
-	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
+	@ECHO This batch has been run in DEMO - next command is not going to be executed >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
+	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
 )
 
 IF %DEMO% == 0 (
-	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
+	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG%  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE%"
 )
