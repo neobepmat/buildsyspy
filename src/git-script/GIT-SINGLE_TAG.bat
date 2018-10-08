e4c9a59ad0ba178a3ad33492a8871035aba4fc63
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-08 12:31:24 +0200

    chg removed unused ECHO

--- src/git-script/GIT-SINGLE_TAG.bat
+++ src/git-script/GIT-SINGLE_TAG.bat
@@ -6,12 +6,10 @@ call "%GIT_EXE%" -C %CURRENT_FOLDER% checkout %BRANCH_TO_TAG_INTERNAL% >> "%EXT_
 @ECHO Now Tagging >> "%EXT_LOG_FILE%" 2>>1& 
 
 IF %DEMO% == 1 (
-	@ECHO -------- Script is in DEMO status --------------------------------------------------- >> "%EXT_LOG_FILE%" 2>>1& 
 	@ECHO This batch has been run in DEMO - next command is not going to be executed >> "%EXT_LOG_FILE%" 2>>1& 
-	@ECHO "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
+	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
 )
 
 IF %DEMO% == 0 (
 	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG%  >> "%EXT_LOG_FILE%" 2>>1& 
 )
-rem git tag -a v1.4 -m "my version 1.4"
