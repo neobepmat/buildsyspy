fdcca2579c3a539c5e7fbb495f37acc31302b5dc
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-08 14:40:34 +0200

    fix msg added for new tag

--- src/git-script/GIT-SINGLE_TAG.bat
+++ src/git-script/GIT-SINGLE_TAG.bat
@@ -1,3 +1,5 @@
+SET MSG_NEW_TAG=new version
+
 @echo --------------------->> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
 @echo Checking out %CURRENT_REPO% >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
 @echo --------------------->> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
@@ -7,11 +9,11 @@ call "%GIT_EXE%" -C %CURRENT_FOLDER% checkout %BRANCH_TO_TAG_INTERNAL% >> "%EXT_
 
 IF %DEMO% == 1 (
 	@ECHO This batch has been run in DEMO - next command is not going to be executed >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
-	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag %TAG%  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
+	@ECHO call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG% -m "%MSG_NEW_TAG%"  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
 )
 
 IF %DEMO% == 0 (
-	@ECHO Creating Tag [%TAG%] for repository [%CURRENT_REPO%] branch [%BRANCH_TO_TAG_INTERNAL%]
-	call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG%  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
+	@ECHO Creating Tag [%TAG%] for repository [%CURRENT_REPO%] branch [%BRANCH_TO_TAG_INTERNAL%] >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
+	call "%GIT_EXE%" -C %CURRENT_FOLDER% tag -a %TAG% -m "%MSG_NEW_TAG%"  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
 	call "%GIT_EXE%" -C %CURRENT_FOLDER% push origin %TAG%  >> "%EXT_LOG_FILE%" 2>>"%EXT_LOG_FILE_ERR%"
 )
