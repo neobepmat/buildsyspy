4aca8c3bcb03a487474f815afb1f51757028be45
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-10-12 10:50:56 +0200

    chg demo disabled for all versions

--- src/11-4/42_DDK_TAG_U_R.bat
+++ src/11-4/42_DDK_TAG_U_R.bat
@@ -7,7 +7,7 @@ set GIT-ROOT-COMMON=c:\git-common
 :: Warning! Always use the slash forward within the version number - the backslash is not allowed
 SET TAG=v11/11.4.1.1
 :: used to start the task as simulated
-SET DEMO=1 
+SET DEMO=0
 SET EXT_LOG_FILE=%GIT-ROOT%\BuildSystem\src\log\GIT-TAG_DDK_11-4_MASTER.log
 SET EXT_LOG_FILE_ERR=%GIT-ROOT%\BuildSystem\src\log\GIT-TAG_DDK_-11-4_MASTER_ERR.log
 SET BRANCH_TO_TAG_INTERNAL=master
