3d626557f94a57b8fb99dddf13ed6c6b4b2f50e1
Angelo Saiani <angelo.saiani@ftsystem.com>
2018-11-20 14:49:28 +0100

    Rilascio ufficiale 11.4.16.1

--- src/11-4/42_FTCM_U_R.bat
+++ src/11-4/42_FTCM_U_R.bat
@@ -1,6 +1,6 @@
 REM =========================================
-REM data di rilascio ufficiale 16-11-2018
-SET VERSIONE=11.4.15.1
+REM data di rilascio ufficiale 20-11-2018
+SET VERSIONE=11.4.16.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master
