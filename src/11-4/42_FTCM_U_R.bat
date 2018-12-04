dd3d82b9275c90ebb9908760c680e04be48580e7
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2018-12-04 10:21:19 +0100

    Rilascio ufficiale 11.4.18.1

--- src/11-4/42_FTCM_U_R.bat
+++ src/11-4/42_FTCM_U_R.bat
@@ -1,6 +1,6 @@
 REM =========================================
-REM data di rilascio ufficiale 03-12-2018
-SET VERSIONE=11.4.17.1
+REM data di rilascio ufficiale 04-12-2018
+SET VERSIONE=11.4.18.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master
