5f56d2aab650178c99372f9c5b1e053ebe39925a
Angelo Saiani <angelo.saiani@ftsystem.com>
2019-01-02 08:12:28 +0100

    rilascio ufficiale 11.10.7.2 - patch

--- src/11-10/42_FTCM_U.bat
+++ src/11-10/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM data di rilascio ufficiale 12-12-2018
-SET VERSIONE=11.10.7.1
+REM data di rilascio ufficiale 02-01-2019 - Patch per problema oscilloscopio
+SET VERSIONE=11.10.7.2
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master-11-10
