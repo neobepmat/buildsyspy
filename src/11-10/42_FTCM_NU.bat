78742b08a93c42b715e48d113d2aa8e9db92beb6
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2019-01-16 10:00:20 +0100

    rilascio non ufficiale 11.10.108.1 - full throttle

--- src/11-10/42_FTCM_NU.bat
+++ src/11-10/42_FTCM_NU.bat
@@ -1,6 +1,6 @@
 REM =========================================
-REM 14-01-2019, rilascio non ufficiale a tutta manetta - full throttle
-SET VERSIONE=11.11.107.0
+REM 16-01-2019, rilascio non ufficiale a tutta manetta - full throttle - take 002
+SET VERSIONE=11.11.108.0
 SET VERSIONE_WITH_TEXT=%VERSIONE%-develop
 SET RILASCIO_UFFICIALE=0
 SET MAIN_TARGET=11-10
