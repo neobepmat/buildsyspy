8085aeea60cf3ef8024b2c763d56b818e5b10d37
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2019-05-02 10:14:45 +0200

    rilascio non ufficiale FTCM 11.11.142.1

--- src/11-10/42_FTCM_NU.bat
+++ src/11-10/42_FTCM_NU.bat
@@ -1,6 +1,6 @@
 REM =========================================
-REM 18-04-2019, rilascio non ufficiale - test Shutdown - modifiche anche a TDK
-SET VERSIONE=11.11.138.1
+REM 02-05-2019, rilascio non ufficiale - test Shutdown dopo ulteriori correzioni e modifiche
+SET VERSIONE=11.11.142.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-develop
 SET RILASCIO_UFFICIALE=0
 SET MAIN_TARGET=11-10
