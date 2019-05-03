cfce459d2f83a34c17580aeff3a7b0e4923921c0
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2019-05-03 12:26:16 +0200

    rilascio ufficiale FTCM 11.10.12.1

--- src/11-10/42_FTCM_U.bat
+++ src/11-10/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM data di rilascio ufficiale 29-03-2019
-SET VERSIONE=11.10.11.1
+REM data di rilascio ufficiale 03-05-2019
+SET VERSIONE=11.10.12.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master-11-10
