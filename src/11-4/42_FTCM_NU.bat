4420d2319970f409af29c55031eb372f8b65ba50
Angelo Saiani <angelo.saiani@ftsystem.com>
2019-05-30 09:27:44 +0200

    Rilascio non ufficiale - 11.5.113.1

--- src/11-4/42_FTCM_NU.bat
+++ src/11-4/42_FTCM_NU.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM 22-05-2019, Sistemata gestione visualizzazione pulsante calibrazione statistica - Aggiunta gestione visualizzazione per pulsante Acquisizione Immagini
-SET VERSIONE=11.5.112.1
+REM 28-05-2019, Possibile soluzione gestione workflow ciclici per LDS su 2 unità
+SET VERSIONE=11.5.113.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-develop
 SET RILASCIO_UFFICIALE=0
 SET MAIN_TARGET=11-4
