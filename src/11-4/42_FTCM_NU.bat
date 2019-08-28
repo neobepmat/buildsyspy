b65d949e6981b9e0fa08b346372dd2ed6932bfcd
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2019-08-28 08:53:10 +0200

    releasing unofficial release for 11-4 / 11.5.115.1

--- src/11-4/42_FTCM_NU.bat
+++ src/11-4/42_FTCM_NU.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM 26-08-2019, 18-189 Bugfixing su Cambio Formato Automatico
-SET VERSIONE=11.5.114.1
+REM 28-08-2019, 18-189 Bugfixing su Cambio Formato Automatico
+SET VERSIONE=11.5.115.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-develop
 SET RILASCIO_UFFICIALE=0
 SET MAIN_TARGET=11-4
