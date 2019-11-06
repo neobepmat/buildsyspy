dfa82edb7e95c719cdd1791b85c65d941adf9922
FTSYSTEM\gfrazzi <giancarlo.frazzi@ftsystem.com>
2019-11-06 11:20:10 +0100

    Rilascio 11.4.25.1

--- src/11-4/42_FTCM_TAG_U.bat
+++ src/11-4/42_FTCM_TAG_U.bat
@@ -1,4 +1,4 @@
-REM Imposta il TAG definito in questo file sui repository MASTER della versione 11.10
+REM Imposta il TAG definito in questo file sui repository MASTER della versione 11.4
 REM IMPORTANTE: questi script non effettuano alcuna PULL e 
 REM debbono essere utilizzati dopo aver eseguito un setup ufficiale con successo
 
@@ -7,7 +7,7 @@ NET USE Z: \\VBOXSRV\C_DRIVE
 SET GIT-ROOT=Z:\GIT
 set GIT-ROOT-COMMON=Z:\GIT
 :: Warning! Always use the slash forward within the version number - the backslash is not allowed
-SET TAG=v11/11.4.21.1
+SET TAG=v11/11.4.25.1
 :: used to start the task as simulated
 SET DEMO=0
 SET EXT_LOG_FILE=%GIT-ROOT%\BuildSystem\src\log\GIT-TAG_FTCM_11-4_MASTER.log
