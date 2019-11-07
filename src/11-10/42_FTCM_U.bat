2770d8edb0d507bd5f8416ef5b25fa07555ddfc9
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2019-11-07 15:14:16 +0100

    releasing official release for TDK 11-10 / 11.10.21.1

--- src/11-10/42_FTCM_U.bat
+++ src/11-10/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM data di rilascio ufficiale 05-11-2019
-SET VERSIONE=11.10.20.1
+REM data di rilascio ufficiale 07-11-2019
+SET VERSIONE=11.10.21.1
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master-11-10
