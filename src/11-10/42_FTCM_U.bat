eb8b7d06993c0148b89e7ce33446cedc4671b5fa
Angelo Saiani <angelo.saiani@ftsystem.com>
2019-11-20 10:11:04 +0100

    11.10 - Rilascio ufficiale 11.10.22.2 (patch)

--- src/11-10/42_FTCM_U.bat
+++ src/11-10/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM data di rilascio ufficiale 15-11-2019
-SET VERSIONE=11.10.22.1
+REM data di rilascio ufficiale 20-11-2019
+SET VERSIONE=11.10.22.2
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=1
 SET BRANCH_TO_CHECKOUT=master-11-10
