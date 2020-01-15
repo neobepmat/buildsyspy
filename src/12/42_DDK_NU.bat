2f132d3a15b85b72063e93b386f2cb9a38fbdaaf
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-01-15 14:50:31 +0100

    fixing version number for setup dkk

--- src/12/42_DDK_NU.bat
+++ src/12/42_DDK_NU.bat
@@ -20,14 +20,14 @@ MKDIR "%BATCH_FOLDER%\log"
 SET LOG_FILE=%BATCH_FOLDER%\log\SETUP_DDK_12_develop.LOG
 SET LOG_FILE_ERR=%BATCH_FOLDER%\log\SETUP_DDK_ERR_12_develop.LOG
 
-SET DEVICE_DRIVER_VERSION=%VERSIONE%-develop
+SET DEVICE_DRIVER_VERSION_WITH_TEXT=%VERSIONE%-develop
 SET PATH_MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBUILD.EXE
 SET WIX_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.targets
 SET WIXCA_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.ca.targets
 
 REM PULISCE LE CARTELLE IN CUI VERRANNO COPIATI I FILES, CANCELLA I FILES DI LOG 
 ECHO Inizializzazione log per generazione setup TDK Device Driver > %LOG_FILE% 2>%LOG_FILE_ERR%
-ECHO Sto generando la versione [%DEVICE_DRIVER_VERSION%]
+ECHO Sto generando la versione [%DEVICE_DRIVER_VERSION_WITH_TEXT%]
 
 CALL "%BATCH_FOLDER%\common\42_DDK.BAT"
 
--- src/12/42_DDK_U.bat
+++ src/12/42_DDK_U.bat
@@ -18,14 +18,14 @@ MKDIR "%BATCH_FOLDER%\log"
 SET LOG_FILE=%BATCH_FOLDER%\log\SETUP_DDK_12_master.LOG
 SET LOG_FILE_ERR=%BATCH_FOLDER%\log\SETUP_DDK_ERR_12_master.LOG
 
-SET DEVICE_DRIVER_VERSION=%VERSIONE%-master
+SET DEVICE_DRIVER_VERSION_WITH_TEXT=%VERSIONE%-master
 SET PATH_MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBUILD.EXE
 SET WIX_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.targets
 SET WIXCA_TARGETSPATH=C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.ca.targets
 
 REM PULISCE LE CARTELLE IN CUI VERRANNO COPIATI I FILES, CANCELLA I FILES DI LOG 
 ECHO Inizializzazione log per generazione setup TDK Device Driver > %LOG_FILE% 2>%LOG_FILE_ERR%
-ECHO Sto generando la versione [%DEVICE_DRIVER_VERSION%]
+ECHO Sto generando la versione [%DEVICE_DRIVER_VERSION_WITH_TEXT%]
 
 CALL "%BATCH_FOLDER%\common\42_DDK.BAT"
 
