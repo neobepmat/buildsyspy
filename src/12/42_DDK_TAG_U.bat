58b084e770af07fe65af4fb65f00323e7e12682b
Angelo Saiani <angelo.saiani@ftsystem.com>
2020-03-02 09:49:33 +0100

    Tag file updated

--- src/12/42_DDK_TAG_U.bat
+++ src/12/42_DDK_TAG_U.bat
@@ -5,8 +5,8 @@ REM debbono essere utilizzati dopo aver eseguito un setup ufficiale con successo
 SET TEMP_DRIVE_LETTER=K:
 NET USE %TEMP_DRIVE_LETTER% \\VBOXSRV\C_DRIVE
 
-SET GIT-ROOT=K:\GIT
-set GIT-ROOT-COMMON=k:\GIT
+SET GIT-ROOT=%TEMP_DRIVE_LETTER%\GIT
+set GIT-ROOT-COMMON=%TEMP_DRIVE_LETTER%\GIT
 :: Warning! Alays use the slash forward within the version number - the backslash is not allowed
 SET TAG=v12/1.2.0.0
 :: used to start the task as simulated
@@ -15,7 +15,7 @@ SET EXT_LOG_FILE=%GIT-ROOT%\bitbucket-ft\BuildSystem\src\log\GIT-TAG_DDK_12_MAST
 SET EXT_LOG_FILE_ERR=%GIT-ROOT%\bitbucket-ft\BuildSystem\src\log\GIT-TAG_DDK_12_MASTER_ERR.log
 SET BRANCH_TO_TAG_INTERNAL=master
 
-CALL "%GIT-ROOT%\BuildSystem\src\git-script\GIT-TAG-MASTER-DDK.bat"
+CALL "%GIT-ROOT%\bitbucket-ft\BuildSystem\src\git-script\GIT-TAG-MASTER-DDK.bat"
 
 REM RIMOZIONE MAPPING TEMPORANEO
 NET USE %TEMP_DRIVE_LETTER% /DELETE
