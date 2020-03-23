189cc095bdf159eac46145979fb1144422c9555e
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-03-23 15:16:55 +0100

    releasing unofficial ddk 101.0.12.0

--- src/12/42_DDK_NU.bat
+++ src/12/42_DDK_NU.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM 06-03-2020, rilascio interno per test Calibrazione VisionPro Tropicana
-SET VERSIONE=101.0.11.0
+REM 21-03-2020, rilascio interno per test Calibrazione VisionPro Tropicana
+SET VERSIONE=101.0.12.0
 SET RILASCIO_UFFICIALE=0
 SET CONFIGURATION=Release
 REM =========================================
