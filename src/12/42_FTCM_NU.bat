b2d82017453f0b6a7ed323553ce61d80ef34a98b
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-04-27 10:40:09 +0200

    releasing unofficial HMI 101.0.34

--- src/12/42_FTCM_NU.bat
+++ src/12/42_FTCM_NU.bat
@@ -1,6 +1,6 @@
 REM =========================================
-REM 22-04-2020, Rilascio non ufficiale per Test Bottle in Test Management
-SET VERSIONE=101.0.33.0
+REM 27-04-2020, Rilascio non ufficiale per Test Bottle in Test Management - include issue FTCM12-32
+SET VERSIONE=101.0.34.0
 SET VERSIONE_WITH_TEXT=%VERSIONE%-develop
 SET RILASCIO_UFFICIALE=0
 SET MAIN_TARGET=12
