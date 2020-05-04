561f2227f7e7a081d47c66f50a1f979c83c85a85
Angelo Saiani <angelo.saiani@ftsystem.com>
2020-05-04 14:13:46 +0200

    Official release HMI 1.10.0.0

--- src/12/42_FTCM_U.bat
+++ src/12/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM 20-04-2020: Rilascio ufficiale
-SET VERSIONE=1.9.0.0
+REM 04-05-2020: Rilascio ufficiale
+SET VERSIONE=1.10.0.0
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=0
 SET BRANCH_TO_CHECKOUT=master
