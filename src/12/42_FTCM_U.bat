ab2bf4759a1ceef6ea799b3416f3c63410e0f8d8
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-06-16 11:53:25 +0200

    releasing official HMI 1.12.0.0

--- src/12/42_FTCM_U.bat
+++ src/12/42_FTCM_U.bat
@@ -1,6 +1,6 @@
 ﻿REM =========================================
-REM 13-05-2020: Rilascio ufficiale
-SET VERSIONE=1.11.0.0
+REM 16-06-2020: Rilascio ufficiale
+SET VERSIONE=1.12.0.0
 SET VERSIONE_WITH_TEXT=%VERSIONE%-master
 SET DO_GIT_CHECKOUT=0
 SET BRANCH_TO_CHECKOUT=master
