83c46d618541ff36e5d30d59477a6e5fef98ba7f
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-07-13 10:58:54 +0200

    adding execute call for ssh agent

--- src/git-runner/git-runner.bat
+++ src/git-runner/git-runner.bat
@@ -1,6 +1,8 @@
 REM Lista dei vari repo con versioni per BuildSystem
 SET SCRIPT_UPDATER=c:\git\buildsystem\src\git-runner\git-updater.bat
 
+call "c:\Program Files\Git\cmd\start-ssh-agent.cmd"
+
 SET GIT_EXE=C:\Program Files\Git\cmd\git.exe
 
 SET GIT_ROOT=C:\GIT-12
