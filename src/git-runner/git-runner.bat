e2c1373b7029d5544ecd40a6fec2cf35c378f7f5
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-07-13 10:01:34 +0200

    updating updater and runner for git/buildsystem

--- src/git-runner/git-runner.bat
+++ src/git-runner/git-runner.bat
@@ -2,10 +2,12 @@ REM Lista dei vari repo con versioni per BuildSystem
 SET SCRIPT_UPDATER=git-updater.bat
 
 SET GIT_ROOT=C:\GIT-12
+SET GIT_FOLDER=%GIT_ROOT%/bitbucket-ft/BuildSystem
 SET BRANCH_TO_CHECKOUT=develop
 CALL %SCRIPT_UPDATER% %GIT_ROOT% %BRANCH_TO_CHECKOUT% 
 
 SET GIT_ROOT=C:\GIT-12-beta
+SET GIT_FOLDER=%GIT_ROOT%/bitbucket-ft/BuildSystem
 SET BRANCH_TO_CHECKOUT=develop
 CALL %SCRIPT_UPDATER% %GIT_ROOT% %BRANCH_TO_CHECKOUT% 
 
--- src/git-runner/git-updater.bat
+++ src/git-runner/git-updater.bat
@@ -3,13 +3,7 @@
 SET LOCAL_BRANCH_TO_CHECKOUT=develop
 IF NOT [%BRANCH_TO_CHECKOUT%] == [] SET LOCAL_BRANCH_TO_CHECKOUT=%BRANCH_TO_CHECKOUT%
 
-SET GIT_ROOT_INTERNAL=C:\GIT-11-4
-IF NOT [%GIT_ROOT%] == [] SET GIT_ROOT_INTERNAL=%GIT_ROOT%
-
 rem ------------------------------------------------------------------------
-
-SET GIT_EXE=C:\Program Files\Git\cmd\git.exe
-SET GIT_FOLDER=%GIT_ROOT_INTERNAL%/BuildSystem
 
 @echo Starting Git Checkout...
 
