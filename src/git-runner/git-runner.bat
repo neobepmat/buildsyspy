aeb1b307e0d24bc987be79b64c7e20899e1a1fcc
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-07-13 10:05:09 +0200

    adding full path to git

--- src/git-runner/git-runner.bat
+++ src/git-runner/git-runner.bat
@@ -1,6 +1,8 @@
 REM Lista dei vari repo con versioni per BuildSystem
 SET SCRIPT_UPDATER=git-updater.bat
 
+SET GIT_EXE=C:\Program Files\Git\cmd\git.exe
+
 SET GIT_ROOT=C:\GIT-12
 SET GIT_FOLDER=%GIT_ROOT%/bitbucket-ft/BuildSystem
 SET BRANCH_TO_CHECKOUT=develop
