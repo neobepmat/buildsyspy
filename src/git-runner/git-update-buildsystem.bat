f2ce6d59b84568b9fb873100822e57202209793a
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-07-13 11:09:19 +0200

    executing git-runner from git-buildystem-updater

--- src/git-runner/git-update-buildsystem.bat
+++ src/git-runner/git-update-buildsystem.bat
@@ -3,6 +3,7 @@ REM Lista dei vari repo con versioni per BuildSystem
 SET BUILDSYSTEM_FOLDER=c:\git\buildsystem
 SET GIT_EXE=C:\Program Files\Git\cmd\git.exe
 SET BRANCH_TO_CHECKOUT=develop
+SET GIT_RUNNER=%BUILDSYSTEM_FOLDER%/src/git-runner/git-runner.bat
 
 call "c:\Program Files\Git\cmd\start-ssh-agent.cmd"
 
@@ -11,5 +12,7 @@ cd %BUILDSYSTEM_FOLDER%
 call "%GIT_EXE%" checkout %BRANCH_TO_CHECKOUT%
 call "%GIT_EXE%" pull
 call "%GIT_EXE%" status
+
+call %GIT_RUNNER%
 
 PAUSE
