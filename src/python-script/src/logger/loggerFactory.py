85d52254e7d775fbf451961ea252f457e98429b0
Giuseppe Grilli <giuseppe.grilli@ftsystem.com>
2020-07-29 16:15:52 +0200

    completing logger factory

--- src/python-script/src/logger/loggerFactory.py
+++ src/python-script/src/logger/loggerFactory.py
@@ -1,13 +1,22 @@
 import logging
 
-logFilename = 'pylog/mylog.log'
+internalLogFilename = '../pylog/mylog.log'
+internalClassName = 'NullClass'
 
-def getLog(className):
-	logger = logging.getLogger(className)
-	logger.setLevel(logging.DEBUG)
-	fh = logging.FileHandler(logFilename)
-	fh.setLevel(logging.DEBUG)
-	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-	fh.setFormatter(formatter)
-	logger.addHandler(fh)
-	return logger
+def getLog(className=None, logFilename=None, logLevel=logging.DEBUG):
+
+    if className is None:
+        className = internalClassName
+
+    logger = logging.getLogger(className)
+    logger.setLevel(logging.DEBUG)
+
+    if logFilename is None:
+        logFilename = internalLogFilename
+
+    fh = logging.FileHandler(logFilename)
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
